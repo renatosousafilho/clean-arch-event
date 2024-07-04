0f0d66f5579ff94e8339e42b6c8eb9176f567eab
Renato Filho <renatosousafilho@gmail.com>
2024-07-04 14:55:28 -0300

    test: cria testes unitários de RepositoryFactory

---
+++ src/infra/repository/RepositoryFactory.test.ts
@@ -0,0 +1,37 @@
+import RepositoryFactory from './RepositoryFactory';
+import { Mysql2Adapter, PgPromiseAdapter } from '../database/DatabaseConnection';
+import { RepositoryFactoryDatabase } from './RepositoryFactory';
+import { TicketRepositoryMySQL, TicketRepositoryPostgres } from './TicketRepository';
+import { EventRepositoryMySQL, EventRepositoryPostgres } from './EventRepository';
+
+test('When createTicketRepository is called with Postgres, it should return an instance of TicketRepositoryPostgres', async () => {
+  const databaseConnection = new PgPromiseAdapter();
+  const repositoryFactory = new RepositoryFactoryDatabase(databaseConnection);
+  const ticketRepository = repositoryFactory.createTicketRepository();
+  expect(ticketRepository).toBeInstanceOf(TicketRepositoryPostgres);
+  await databaseConnection.close();
+});
+
+test('When createTicketRepository is called with MySQL, it should return an instance of TicketRepositoryMySQL', async () => {
+  const databaseConnection = new Mysql2Adapter();
+  const repositoryFactory = new RepositoryFactoryDatabase(databaseConnection);
+  const ticketRepository = repositoryFactory.createTicketRepository();
+  expect(ticketRepository).toBeInstanceOf(TicketRepositoryMySQL);
+  await databaseConnection.close();
+});
+
+test('When createEventRepository is called with Postgres, it should return an instance of EventRepositoryPostgres', async () => {
+  const databaseConnection = new PgPromiseAdapter();
+  const repositoryFactory = new RepositoryFactoryDatabase(databaseConnection);
+  const eventRepository = repositoryFactory.createEventRepository();
+  expect(eventRepository).toBeInstanceOf(EventRepositoryPostgres);
+  await databaseConnection.close();
+});
+
+test('When createEventRepository is called with Postgres, it should return an instance of EventRepositoryMySQL', async () => {
+  const databaseConnection = new Mysql2Adapter();
+  const repositoryFactory = new RepositoryFactoryDatabase(databaseConnection);
+  const eventRepository = repositoryFactory.createEventRepository();
+  expect(eventRepository).toBeInstanceOf(EventRepositoryMySQL);
+  await databaseConnection.close();
+});
